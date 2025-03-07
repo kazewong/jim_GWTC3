520e5a50eb809eb8aa44f5e48c4b93962e7fdfaf
Kaze Wong <kazewong.physics@gmail.com>
2025-03-07 09:57:19 -0500

    Moved assets

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ This repository contains the results of the comparison between jim and bilby for
 Figures are stored in the [figures](figures) directory.
 
 The current status of the events can be found in [event_status.csv](event_status.csv).
+
+This repository contains the results of the comparison between jim and bilby for the GWTC-2.1 and GWTC-3 events.
+Figures are stored in the [figures](figures) directory.
+
+The current status of the events can be found in [event_status.csv](event_status.csv).
---
+++ src/static/bilby_runs/submit_jobs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Activate the conda environment
+source /home/thomas.ng/.bashrc
+conda activate /home/sylvia.biscoveanu/.conda/envs/bilby_o4review_230314/
+
+# Read the event IDs from the CSV file
+csv_file="/home/thomas.ng/project/jim_GWTC3/event_status.csv"
+gw_ids=$(awk -F, 'NR>1 {print $1}' $csv_file)
+
+# Loop over each GW event ID
+for gw_id in $gw_ids
+do
+  # Define the result directory path
+  result_dir="/home/thomas.ng/project/jim_GWTC3/bilby_runs/outdir"
+  
+  # Check if the result directory contains a subdirectory named as the GW ID
+  if [ -d "$result_dir/$gw_id" ]; then
+    echo "Result already exists for $gw_id, skipping submission."
+    sleep 0.5
+    continue
+  fi
+
+  # Submit the job and wait for it to finish
+  bilby_pipe /home/thomas.ng/project/jim_GWTC3/bilby_runs/config/${gw_id}_config.ini --submit
+  wait
+  
+  echo "Submitted job for $gw_id"
+done
+
+# Deactivate the conda environment
+conda deactivate
